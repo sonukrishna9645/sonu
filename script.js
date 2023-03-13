ae6a274a5d49b70fe049114a8017ead4c30bf86a
UdithDas <udithdaskm@gmail.com>
2023-03-13 02:41:30 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,86 @@
+// var sum=5+10;
+// console.log(sum);
+// var product=5*2;
+// console.log(product);
+// var sub=5-2;
+// console.log(sub);
+// var rem=5/2;
+// console.log(rem);
+// document.write("sum="+sum)
+// document.write("product="+product)
+// document.write("sub="+sub)
+// document.write("rem="+rem)
+// document.write(`sum is ${sum}`)
+// var students={
+//     name:"sonu",
+//     age:19,
+// place:"tarur" 
+// }
+// console.log(students);
+// console.log(students.name);
+// var cars={
+//     model:"porche s50 spider",
+//     colur:"offwhite",
+//     price:30000000
+// }
+// console.log(cars);
+// console.log(cars.price)
+var fruits=["apple","banana","orange",10]
+fruits[2]="grapes"
+
+// console.log(fruits[3]);
+// console.log(fruits.length);
+//fruits.push("plum");
+
+// console.log(fruits);
+// console.log(fruits);
+// fruits.unshift("xyz")
+// fruits.pop();
+function add(a,b){
+    var c=a+b;
+    console.log(c);
+
+}
+// add(1,2);
+// add(2,3);
+
+// const addition =(a,b)=>{
+//     var c=a+b;
+//     console.log(c);
+
+// }
+// addition(1,2)
+// const multiplication =(a,b)=>{
+//     var c=a*b;
+//     console.log(c);
+
+// }
+// multiplication(1,20)
+// var x=20;
+// if(x>30){
+//     console.log("good");
+
+// }
+// else
+// console.log("bad");
+// for(var i=1;i<=5;i++)
+// {
+//     console.log(i);
+// }
+// var fruits=["apple","orange","greenapple","grapes"]
+// for(i=0;i<fruits.length;i++){
+//     console.log(fruits[i])
+// }
+
+// let p=0;
+// while(p<fruits.length){
+//     console.log(fruits[p]);
+//     p++;
+// }
+
+// let i=0;
+// do {
+//     console.log(fruits[i])
+//     i++
+    
+// } while (i<fruits.length);
